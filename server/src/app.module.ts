36ed3f01e199968106ee4489ff59c7f8afad3380
0xVector <hello@0xvector.me>
2024-04-27 13:27:56 +0200

    server: Refactor ordering of imports

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from "@nestjs/common";
+import { EventEmitterModule } from "@nestjs/event-emitter";
+import { ScheduleModule } from "@nestjs/schedule";
+import * as winston from "winston";
+import { WinstonModule, utilities } from "nest-winston";
 import { AppController } from "./app.controller";
 import { CoreModule } from "./core/core.module";
-import { ScheduleModule } from "@nestjs/schedule";
 import { ServerModule } from "./server/server.module";
 import { UpdaterModule } from "./updater/updater.module";
-import { EventEmitterModule } from "@nestjs/event-emitter";
-import { WinstonModule, utilities } from "nest-winston";
-import * as winston from "winston";
 
 /** The root module of the application */
 @Module({
